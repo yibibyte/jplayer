5fc225a4e2e1a4d25af22de351bc192490ed385b
il <>
2024-02-07 02:09:32 +0300

    add functional for MediaPlayer

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module ru.music.jplayer {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires javafx.media;
+    requires javafx.web;
 
     opens ru.music.jplayer to javafx.fxml;
     exports ru.music.jplayer;
--- src/main/java/ru/music/jplayer/MediaApp.java
+++ src/main/java/ru/music/jplayer/MediaApp.java
@@ -1,13 +1,24 @@
 package ru.music.jplayer;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class MediaApp extends Application {
 
@@ -18,12 +29,29 @@ public class MediaApp extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        String mediaFile = "/symphonyNo40.mp3"; // Путь к вашему медиа-файлу
-        Media media = new Media(mediaFile);
-        mediaPlayer = new MediaPlayer(media);
+    public void start(Stage stage) throws MalformedURLException {
 
-        MediaView mediaView = new MediaView(mediaPlayer);
+        PieChart chart = new PieChart();
+        chart.getData().addAll(
+                new PieChart.Data("Java", 20.0),
+                new PieChart.Data("Python", 15.0),
+                new PieChart.Data("JavaScript", 12.0),
+                new PieChart.Data("C#", 10.0),
+                new PieChart.Data("C++", 8.0),
+                new PieChart.Data("Ruby", 5.0),
+                new PieChart.Data("Other", 30.0));
+        chart.setTitle("Programming Language Statistics");
+
+        String location = "http://demo2s.com";
+        WebView browser = new WebView();
+        WebEngine engine = browser.getEngine();
+        engine.load(location);
+
+        Path path = Paths.get("mama-ya-popuz-yana.mp4");
+        Media media = new Media(path.toUri().toString());
+        MediaPlayer player = new MediaPlayer(media);
+        player.setAutoPlay(true);
+        MediaView video = new MediaView(player);
         Button playButton = new Button("Play");
         Button pauseButton = new Button("Pause");
         Button stopButton = new Button("Stop");
@@ -31,22 +59,32 @@ public class MediaApp extends Application {
         playButton.setOnAction(e -> mediaPlayer.play());
         pauseButton.setOnAction(e -> mediaPlayer.pause());
         stopButton.setOnAction(e -> mediaPlayer.stop());
+        video.setOnError(ex -> System.out.println(ex));
 
-        HBox controlBox = new HBox(10, playButton, pauseButton, stopButton);
-        HBox.setMargin(playButton, new javafx.geometry.Insets(10, 10, 10, 10));
+        stage.setWidth(500);
+        stage.setHeight(500);
+        stage.setScene(new Scene(browser));
+        stage.show();
 
-        HBox root = new HBox(10, mediaView, controlBox);
-        Scene scene = new Scene(root, 800, 600);
+        Stage stage2 = new Stage();
+        stage2.setWidth(500);
+        stage2.setHeight(500);
+        stage2.setX(stage.getX() + stage.getWidth());
+        stage2.setY(stage.getY());
+        stage2.setScene(new Scene(chart));
+        stage2.show();
 
-        primaryStage.setTitle("Media Player");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        VBox box = new VBox(10, video, new HBox(10,  playButton, pauseButton, stopButton));
+        box.setAlignment(Pos.CENTER);
+        Stage stage3 = new Stage();
+        stage3.setWidth(500);
+        stage3.setHeight(500);
+        stage3.setX(stage.getX());
+        stage3.setY(stage.getY() + stage.getHeight());
+        stage3.setScene(new Scene(box));
+        stage3.show();
+
+
     }
 
-    @Override
-    public void stop() {
-        if (mediaPlayer != null) {
-            mediaPlayer.dispose();
-        }
-    }
 }
